77a01b03c20bb9641b373e371e895b50bb22ae12
Vikas sharma <vikas@sovtechnologies.com>
2022-12-04 01:35:46 +0530

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <img src="https://m.media-amazon.com/images/I/810Jo9IhfsL._SX3000_.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Header.css'
+
+// Icons
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <img src="https://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="logo" className='header__logo' />
+
+                <div className="header__serach">
+                    <input type="text" name="" id="" />
+                    <SearchIcon className='header__searchIcon'/>
+                </div>
+
+                <div className="header__nav">
+                    <div className="header__option">
+                        <span className='header__optionone'>Hello Vikas</span>
+                        <span className='header__optiontwo'>Sign In</span>
+                    </div>
+
+                    <div className="header__option">
+                        <span className='header__optionone'>Return</span>
+                        <span className='header__optiontwo'>& Orders</span>
+                    </div>
+
+                    <div className="header__option">
+                        <span className='header__optionone'>Your</span>
+                        <span className='header__optiontwo'>Prime</span>
+                    </div>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingCartIcon />
+
+                    <span>10</span>
+                </div>
+
+            </div>
+
+            <div className="header__buttom">
+                <ul>
+                    <li>All</li>
+                    <li>Mobile</li>
+                    <li>Category</li>
+                    <li>Computer</li>
+                    <li>Cloths</li>
+                    <li>Shoes</li>
+                    <li>Electronocs</li>
+
+                    <li><img src="https://m.media-amazon.com/images/G/31/img17/Home/AmazonTV/Ravina/Desktop/SWM_400x39_Crushed-S2._CB620377409_.jpg" alt="img" /></li>
+                </ul>
+            </div>
+
+        </>
+    )
+}
+
+export default Header
