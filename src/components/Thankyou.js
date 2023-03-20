0290bfd6941c37c620a2d965a7cb69fc4a9fff3f
Vikas sharma <vikassharma85109@gmail.com>
2023-03-20 10:01:41 +0530

    init

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Thankyou = () => {
- const navigate= useNavigate()
+  const navigate = useNavigate()
   return (
-      <div className='order__comfirm'>
-          <h1>Hello Vikas Sharma</h1> 
-          <h2>Thank you for your order!</h2>
-          <h3>Your Order Number is : 101</h3>
-          <h4>Order Total : 2400</h4>
+    <div className='order__comfirm'>
+      <h1>Hello Vikas Sharma</h1>
+      <h2>Thank you for your order!</h2>
+      <h3>Your Order Number is : 101</h3>
+      <h4>Order Total : 2400</h4>
       <button onClick={() => navigate('/')}>Continue Shopping</button>
     </div>
   )
